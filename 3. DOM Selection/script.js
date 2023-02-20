4377f138da33e3bc6b65753f7c4f921396ccd22d
hidetoraldy <hidetoraldy@gmail.com>
2023-02-20 11:03:23 +0700

    complete script

--- 3. DOM Selection/script.js
+++ 3. DOM Selection/script.js
@@ -16,3 +16,16 @@ h1.style.fontStyle = 'italic';
 // HTMLCollection
 const p1 = document.getElementsByClassName('p1');
 p1[0].innerHTML = 'diubah dari javascript';
+
+//* document.querySelector() -> hanya menangkap 1 element
+const p4 = document.querySelector('#b p');
+p4.style.color = 'blue';
+p4.style.fontSize = '20px';
+
+const li2 = document.querySelector('section#b ul li:nth-child(2)');
+li2.style.backgroundColor = 'orange';
+
+//* document.querySelectorAll()
+// NodeList
+const pi = document.querySelectorAll('p');
+p[2].style.backgroundColor = 'purple';
