27b0b61f7ea2a15bed1237ebc1459df12e49a34b
hidetoraldy <hidetoraldy@gmail.com>
2023-02-20 14:56:02 +0700

    menambahkan tombol acak warna

--- 6. Latihan DOM/script.js
+++ 6. Latihan DOM/script.js
@@ -3,3 +3,16 @@ const tombol = document.getElementById('tombol');
 tombol.onclick = function(){
     document.body.style.backgroundColor = 'darkblue';
 }
+
+const tombolRandom = document.createElement('button');
+const teksTombol = document.createTextNode('Acak Warna!');
+tombolRandom.appendChild(teksTombol);
+tombolRandom.setAttribute('type', 'button');
+tombol.after(tombolRandom);
+
+tombolRandom.addEventListener('click', function(){
+    const red = Math.round(Math.random() * 255 + 1);
+    const green = Math.round(Math.random() * 255 + 1);
+    const blue = Math.round(Math.random() * 255 + 1);
+    document.body.style.backgroundColor = 'rgb(' + red + ','  + green + ',' + blue +')';
+})
