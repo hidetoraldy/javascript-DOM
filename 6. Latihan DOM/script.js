ea0352b27de10f374cf749c1f1b726675d5ac4a1
hidetoraldy <hidetoraldy@gmail.com>
2023-02-20 15:16:47 +0700

    menambahkan slider pengubah warna

--- 6. Latihan DOM/script.js
+++ 6. Latihan DOM/script.js
@@ -1,7 +1,7 @@
 const tombol = document.getElementById('tombol');
 
 tombol.onclick = function(){
-    document.body.style.backgroundColor = 'darkblue';
+    document.body.classList.toggle('hijau-muda');
 }
 
 const tombolRandom = document.createElement('button');
@@ -16,3 +16,27 @@ tombolRandom.addEventListener('click', function(){
     const blue = Math.round(Math.random() * 255 + 1);
     document.body.style.backgroundColor = 'rgb(' + red + ','  + green + ',' + blue +')';
 })
+
+const sMerah = document.querySelector('input[name=sMerah]');
+const sHijau = document.querySelector('input[name=sHijau]');
+const sBiru = document.querySelector('input[name=sBiru]');
+
+sMerah.addEventListener('input', function(){
+    const red = sMerah.value;
+    const green = sHijau.value;
+    const blue = sBiru.value;
+    document.body.style.backgroundColor = 'rgb(' + red + ','  + green + ',' + blue +')';
+})
+sHijau.addEventListener('input', function(){
+    const red = sMerah.value;
+    const green = sHijau.value;
+    const blue = sBiru.value;
+    document.body.style.backgroundColor = 'rgb(' + red + ','  + green + ',' + blue +')';
+})
+sBiru.addEventListener('input', function(){
+    const red = sMerah.value;
+    const green = sHijau.value;
+    const blue = sBiru.value;
+    document.body.style.backgroundColor = 'rgb(' + red + ','  + green + ',' + blue +')';
+})
+
