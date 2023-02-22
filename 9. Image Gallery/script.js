a3b86dbd57dd84959cf388614c7d3d84126288c3
hidetoraldy <hidetoraldy@gmail.com>
2023-02-22 11:48:44 +0700

    finalized

--- 9. Image Gallery/script.js
+++ 9. Image Gallery/script.js
@@ -1,8 +1,24 @@
 const container = document.querySelector('.container');
 const big = document.querySelector('.big');
+const thumbs = document.querySelectorAll('.thumb');
 
 container.addEventListener('click', function(e){
     if(e.target.className == 'thumb'){
         big.src = e.target.src;
+        big.classList.add('fade');
+
+        setTimeout(function(){
+            big.classList.remove('fade');
+        }, 500)
+
+        thumbs.forEach(function(thumb){
+            // if(thumb.classList.contains('active')){
+            //     thumb.classList.remove('active');
+            // }
+            thumb.className = 'thumb';
+        })
+
+        e.target.classList.add('active');
+
     }
 })
